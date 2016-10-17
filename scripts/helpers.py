c8f09b617a2abd6371377d3e448d64fbdf92e43b
Clement Germanicus <clement.germanicus@student.ecp.fr>
2016-10-17 17:22:47 +0200

    Added lab functions, cleaned code and documented the whole thing

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,37 +1,81 @@
 # -*- coding: utf-8 -*-
-"""some helper functions."""
+"""  Some helper functions  """
+
 import numpy as np
 
-def standardize(x, mean_x=None, std_x=None):
-    """Standardize the original data set."""
+
+# Function normalizing a given data set.
+#
+#     x is the data set to standardize
+#     
+#     mean_x is the value we want to subtract to x, default is the mean of x
+#     std_x is the value we want x to be standardized with, default is the standard of x
+# 
+
+"""
+# Returns
+# 
+#     tx (normalized data, with offset if needed)
+#     
+#     mean_x (as supplied, or mean of input x if none was given)
+#     std_x (as supplied, or standard of input x if none was given)
+#
+"""
+
+def normalize (x, offset = False, mean_x = None, std_x = None):
+    
+    # Compute default mean of x
     if mean_x is None:
         mean_x = np.mean(x, axis=0)
-    x = x - mean_x
+        
+    # Subtract the mean
+    tx = x - mean_x
+    
+    # Compute the default standard of x
     if std_x is None:
         std_x = np.std(x, axis=0)
-    x[:, std_x>0] = x[:, std_x>0] / std_x[std_x>0]
+        
+    # Standardize x
+    tx[:, std_x>0] = tx[:, std_x>0] / std_x[std_x>0]
     
-    tx = np.hstack((np.ones((x.shape[0],1)), x))
+    # Add the offset
+    if offset:
+        tx = np.hstack((np.ones((x.shape[0],1)), x))
+    
     return tx, mean_x, std_x
 
 
-def batch_iter(y, tx, batch_size, num_batches=None, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)
-    num_batches_max = int(np.ceil(data_size/batch_size))
+# Function generating a minibatch iterator for a dataset.
+# 
+#     y is the output
+#     tx is the input
+#     
+#     batch_size is the size of the minibatch we want
+#     num_batches is the number of batches we want to perform calculus on
+#
+#     shuffle should be True if we want to pick random values instead of whole blocks of values
+# 
+
+"""
+# Returns
+# 
+#     An iterator over minibatches of our sample y and tx
+#
+"""
+
+def batch_iter (y, tx, batch_size, num_batches = None, shuffle = True):
+    
+    # Compute a default batch count
+    data_size = len (y)
+    num_batches_max = int (np.ceil(data_size/batch_size))
+    
+    # Define the true batch count (lower the one supplied by the user if too big)
     if num_batches is None:
         num_batches = num_batches_max
     else:
-        num_batches = min(num_batches, num_batches_max)
+        num_batches = min (num_batches, num_batches_max)
 
+    # Shuffle the input and output if needed
     if shuffle:
         shuffle_indices = np.random.permutation(np.arange(data_size))
         shuffled_y = y[shuffle_indices]
@@ -39,8 +83,51 @@ def batch_iter(y, tx, batch_size, num_batches=None, shuffle=True):
     else:
         shuffled_y = y
         shuffled_tx = tx
+        
+    # Create the iterator
     for batch_num in range(num_batches):
         start_index = batch_num * batch_size
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+# Function building a polynomial function based on input x
+#
+#     x is the input
+#     degree is the max degree of polynomial values
+#     
+
+"""
+# Returns
+# 
+#     phi (A matrix of polynomial elements from x)
+#
+"""
+
+""" 
+# Example : given [x1, x2, x3] as x and 2 as degree, the output will be:
+#      _          _
+#     | 1  x1  x1² |
+#     | 1  x2  x2² |
+#     |_1  x3  x3²_|
+#
+"""
+            
+def build_poly (x, degree):
+    
+    # First column = only 1s
+    px = [1 for i in range (x.shape [0])]
+    
+    # Initialize output : only 0s yet
+    phi = np.zeros ([x.shape [0], degree + 1])
+    
+    # Fill phi with powers of x
+    for j in range(degree + 1):
+        
+        if j > 0:
+            px = px * x
+            
+        phi[:, j] = px
+        
+    return phi
---
+++ scripts/mlbase.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""  Implementation of the basis for Machine Learning practice.
+
+   It contains all the useful functions, as seen in the Labs, to compute
+   a prediction function, to be used on test data set.
+   
+   To use them, you need to import either mse.py, mae.py or any other python script containing
+   a custom loss function (with its function to compute the gradient), and pass those functions
+   as argument.  """
+
+import numpy as np
+import helpers as hlp
+import mse
+
+
+# Function using gradient descent to compute the weight
+#
+#     y is the train output
+#     tx is the train input
+#     
+#     gamma is the step size to increment w with the gradient (smaller = more preicision but slower convergence)
+#     max_iters is the number of iterations we'll do to find the local minimum
+#     
+#     initial_w is the start weight to go from
+#
+#     loss_function is the loss function to use to compute the loss
+#     gradient_function is the gradient function to use to compute the gradient
+#
+
+"""
+# Returns
+# 
+#     losses (sequence of losses for each weight computed)
+#     ws (sequence of computed weights)
+# 
+"""
+
+def least_squares_GD (y, tx, gamma, max_iters, initial_w = np.zeros (tx.shape [1]), loss_function = compute_mse_loss, gradient_function = compute_mse_gradient):
+    
+    # Initialize w
+    w = initial_w
+    
+    # Define parameters to store w and loss
+    ws = []
+    losses = []
+    
+    # Iterate max_iters times to get closer to the local minimum
+    for n_iter in range(max_iters):
+        
+        # Store current weight
+        ws.append (np.copy (w))
+        
+        # Compute and store current loss
+        loss = loss_function (y, tx, w)
+        losses.append(loss)
+        
+        # Compute gradient for the next iteration
+        if n_iter < max_iters - 1:
+            gw = gradient_function (y, tx, w)
+            w = w - gamma * gw
+
+    return losses, ws
+
+
+# Function using stochastic gradient descent to compute the weight
+#
+#     y is the train output
+#     tx is the train input
+#     
+#     gamma is the step size to increment w with the gradient (smaller = more preicision but slower convergence)
+#     max_iters is the number of iterations we'll do to find the local minimum
+#     
+#     initial_w is the start weight to go from
+#     batch_size is the size of the batch to generate for internal use
+#
+#     loss_function is the loss function to use to compute the loss
+#     gradient_function is the gradient function to use to compute the gradient
+# 
+
+"""
+# Returns
+# 
+#     losses (sequence of losses for each weight computed)
+#     ws (sequence of computed weights)
+# 
+"""
+
+def least_squares_GD (y, tx, gamma, max_iters, initial_w = np.zeros (tx.shape [1]), batch_size = len (y) / 10, loss_function = compute_mse_loss, gradient_function = compute_mse_gradient):
+    
+    # Initialize w
+    w = initial_w
+    
+    # Define parameters to store w and loss
+    ws = []
+    losses = []
+    
+    # Iterate max_iters times to get closer to the local minimum
+    for minibatch_y, minibatch_tx in batch_iter (y, tx, batch_size, max_iters):
+        
+        # Store current weight
+        ws.append (np.copy (w))
+        
+        # Compute and store current loss
+        loss = loss_function (minibatch_y, minibatch_tx, w)
+        losses.append(loss)
+        
+        # Compute gradient for the next iteration
+        if n_iter < max_iters - 1:
+            gw = gradient_function (minibatch_y, minibatch_tx, w)
+            w = w - gamma * gw
+        
+    return losses, ws
+
+
+# Function using Least Squares method to compute the weight (MSE only)
+#
+#     y is the train output
+#     tx is the train input
+# 
+
+"""
+# Returns
+# 
+#     w (the exact weight minimizing the loss function for given y and tx)
+# 
+"""
+
+def least_squares (y, tx):
+    
+    return np.linalg.inv (tx.T.dot (tx)).dot (tx.T).dot (y)
+
+
+# Function using Ridge Regression method to compute the weight (MAE only)
+#
+#     y is the train output
+#     tx is the train input
+#     
+#     lambda_ is the regularization parameter
+# 
+
+"""
+# Returns
+# 
+#     w (the exact weight minimizing the loss function for given y, tx and lambda)
+# 
+"""
+
+def ridge_regression (y, tx, lambda_ = 0):
+    
+    return np.linalg.inv (tx.T.dot (tx) + lambda_ * 2 * tx.shape [0] * np.eye(tx.shape [1])).dot (tx.T).dot (y)
